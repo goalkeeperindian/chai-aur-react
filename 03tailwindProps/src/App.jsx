dce574348d03f2736ab78ef09bb64e6dc10bfd28
goalkeeperindian <lavishchadda098@gmail.com>
2025-09-03 22:41:15 +0530

    Initial commit: added chai-aur-react project

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    userName: "Lavish",
+    age: 21
+  }
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card username="chai-aur-code" btnTxt = "Click Me" />
+      <Card username="Lavish" btnTxt = "Visit me"/>
+    </>
+  )
+}
+
+export default App
